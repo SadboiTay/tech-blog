72dc1a79172831f6921a44d67d7e99d53444a6ca
SadboiTay <tg.hakes@gmail.com>
2021-10-22 19:30:41 -0600

    index and begin home-routes

---
+++ controllers/api/home-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+// get all posts for homepage
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
+
+
